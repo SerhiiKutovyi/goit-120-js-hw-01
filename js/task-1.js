96a1ca38e5d7472a7b6978bed303ca55931474a6
SerhiiKutovyi <wagenrex@gmail.com>
2025-07-10 14:10:11 +0300

    test js

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+const a = 10;
+let b = 5;
+const c = false;
+const d = 5;
+b = '10';
+const set = a + b - c + d;
+
+console.log(set);
+// console.log(Number(set));
+
+// const result = a + b + d;
+
+// console.log(result);
+// console.log(Number(result));
+
+const age = 24;
+const message = 'Poly is ' + age + ' years old!';
+
+console.log(message);
